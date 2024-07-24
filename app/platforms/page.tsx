e965503e708cd58b4f3127f181c9a8ae97e5d595
PavelMedek <medek41@gmail.com>
2024-07-24 14:49:32 +0200

    @ts-ignore

--- app/platforms/page.tsx
+++ app/platforms/page.tsx
@@ -5,6 +5,7 @@ import PageClient from "./PageClient";
 export default function Home() {
   return (
     <PageClient
+      // @ts-ignore
       seriesList={seriesList}
       streamingPlatforms={streamingPlatforms}
     />
