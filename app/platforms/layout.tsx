cbf153dec8534baaab84fca97075e98b3ed205cd
PavelMedek <medek41@gmail.com>
2024-07-24 08:43:49 +0200

    fix

--- app/platforms/layout.tsx
+++ app/platforms/layout.tsx
@@ -210,7 +210,9 @@ const PlatformLayout = ({ children }: { children: React.ReactNode }) => {
     <html lang="en">
       <body className="min-h-screen bg-background font-sans antialiased">
         <LayoutClient
+          /* eslint-disable react/prop-types */
           children={children}
+          /* eslint-enable react/prop-types */
           seriesList={seriesList}
           streamingPlatforms={streamingPlatforms}
         />
